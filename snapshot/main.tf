8bacf9acaa67b1e1274cdf87d7088b7c0e4e7a05
João Taveira Araújo <joao@observeinc.com>
2021-06-04 13:29:50 -0700

    snapshot: add snapshot submodule
    This submodule sets up a recurring EventBridge event which triggers the
    Observe Lambda to snapshot data from AWS API.

---
+++ snapshot/main.tf
@@ -0,0 +1,50 @@
+locals {
+  role_name     = regex(".*role/(?P<role_name>.*)$", var.lambda.lambda_function.role)["role_name"]
+  function_name = regex(".*:function:(?P<function_name>.*)$", var.lambda.lambda_function.arn)["function_name"]
+
+}
+
+resource "aws_iam_policy" "this" {
+  name_prefix = var.iam_name_prefix
+  policy = jsonencode({
+
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = var.action,
+        Effect   = "Allow"
+        Resource = ["*"]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = local.role_name
+  policy_arn = aws_iam_policy.this.arn
+}
+
+resource "aws_cloudwatch_event_rule" "trigger" {
+  name_prefix         = var.eventbridge_name_prefix
+  schedule_expression = var.eventbridge_schedule_expression
+  event_bus_name      = var.eventbridge_schedule_event_bus_name
+}
+
+resource "aws_cloudwatch_event_target" "target" {
+  arn  = var.lambda.lambda_function.arn
+  rule = aws_cloudwatch_event_rule.trigger.name
+  input = jsonencode({
+    snapshot = {
+      include = var.action
+      exclude = var.exclude
+    }
+  })
+}
+
+resource "aws_lambda_permission" "this" {
+  statement_id_prefix = var.statement_id_prefix
+  action              = "lambda:InvokeFunction"
+  principal           = "events.amazonaws.com"
+  function_name       = local.function_name
+  source_arn          = aws_cloudwatch_event_rule.trigger.arn
+}
