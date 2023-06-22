9c26d194f585c0de373813b2e2a4c27e947994f9
João Taveira Araújo <joao@observeinc.com>
2023-06-22 10:37:54 -0700

    fix: update `required_providers` syntax
    We no longer need terraform v0.12 compatibility, update syntax to
    appease tflint.

--- examples/s3_bucket/versions.tf
+++ examples/s3_bucket/versions.tf
@@ -2,7 +2,13 @@ terraform {
   required_version = ">= 1.1.0"
 
   required_providers {
-    aws    = ">= 4.9"
-    random = ">= 3.0.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.9"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
   }
 }
