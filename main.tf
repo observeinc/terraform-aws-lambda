07a908305cfc2c052bb0448b5ec1081871aa55ae
Ben Drucker <ben.drucker@observeinc.com>
2023-07-17 09:19:27 -0700

    fix: declare Lambda's dependency on log group (#68)

--- main.tf
+++ main.tf
@@ -70,6 +70,7 @@ resource "aws_lambda_function" "this" {
   }
 
   depends_on = [
+    aws_cloudwatch_log_group.group,
     aws_iam_role_policy_attachment.lambda_logs,
     aws_iam_role_policy_attachment.vpc_access,
   ]
