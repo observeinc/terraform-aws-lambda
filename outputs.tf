0f616ed3f3c86694d8bb4d55a78af69934e70e55
Ben Drucker <ben.drucker@observeinc.com>
2023-07-17 09:21:54 -0700

    feat: add `log_group_name` output\

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "lambda_function" {
   description = "Observe Lambda function"
   value       = aws_lambda_function.this
 }
+
+output "log_group_name" {
+  description = "The name of the CloudWatch log group where logs for the Lambda will be written."
+  value       = aws_cloudwatch_log_group.group.name
+}
